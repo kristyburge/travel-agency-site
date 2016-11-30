80bec190aa0beafa63c1be2dcff93fc6936d82b2
Kristy Burge <kristyburge@gmail.com>
2016-11-29 19:20:10 -0700

    add support for gulp and npm

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('watch', function(){
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+        // file on computer to watch, the function to do
+  watch('./app/index.html', function(){
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function(){
+    gulp.start('cssInject');
+  });
+
+});
+
+// this task is dependent on the styles task running and completing
+gulp.task('cssInject', ['styles'], function(){
+  return gulp.src('./app/temp/styles/styles.css')
+  .pipe(browserSync.stream());
+});
