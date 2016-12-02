21d2e2ae0a329769fac6a49a0006b5ea2758931d
Kristy Burge <kristyburge@gmail.com>
2016-12-02 15:16:57 -0700

    create get in touch modal

---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,41 @@
+import $ from 'jquery';
+
+class Modal {
+  constructor(){
+    this.openModalButton = $(".open-modal");
+    this.modal = $(".modal");
+    this.closeModalButton = $(".modal__close");
+    this.events();
+  }
+
+  events(){
+    // clicking the open modal button
+    this.openModalButton.click(this.openModal.bind(this));
+    // clicking the x close modal button
+    this.closeModalButton.click(this.closeModal.bind(this));
+    // pushes any key
+    $(document).keyup(this.keyPressHandler.bind(this));
+  }
+
+  keyPressHandler(e){
+    if (e.keyCode == 27){ // 27 is the code for esc key
+      this.closeModal();
+    }
+  }
+
+
+  openModal() {
+    this.modal.addClass("modal--is-visible");
+    return false;/*prevents default behavior of browser scrolling to the top of the page when a link with a # is clicked*/
+  }
+
+  closeModal() {
+
+    this.modal.removeClass("modal--is-visible");
+  }
+
+
+
+}
+
+export default Modal;
