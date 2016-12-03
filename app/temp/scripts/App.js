4da0d197714936613761717ccbcf9d852e45d58b
Kristy Burge <kristyburge@gmail.com>
2016-12-02 18:40:39 -0700

    completed lazyloading of images

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -11218,6 +11218,7 @@
 	  function StickyHeader() {
 	    _classCallCheck(this, StickyHeader);
 
+	    this.lazyImages = (0, _jquery2.default)(".lazyload");
 	    this.siteHeader = (0, _jquery2.default)(".site-header");
 	    this.headerTriggerElement = (0, _jquery2.default)(".large-hero__title");
 	    this.createHeaderWaypoint();
@@ -11225,12 +11226,20 @@
 	    this.headerLinks = (0, _jquery2.default)(".primary-nav a");
 	    this.createPageSectionWaypoints();
 	    this.addSmoothScrolling();
+	    this.refreshWaypoints();
 	  }
 
-	  // enable smooth scrolling with a new method
-
-
 	  _createClass(StickyHeader, [{
+	    key: 'refreshWaypoints',
+	    value: function refreshWaypoints() {
+	      this.lazyImages.on('load', function () {
+	        Waypoint.refreshAll(); // Waypoint library attaches object called Waypoint in the browser's global window scope so note this method refreshes all Waypoints that exist in the browser's memory
+	      });
+	    }
+
+	    // enable smooth scrolling with a new method
+
+	  }, {
 	    key: 'addSmoothScrolling',
 	    value: function addSmoothScrolling() {
 	      this.headerLinks.smoothScroll({ speed: 1500 }); /* speed is in milliseconds*/
