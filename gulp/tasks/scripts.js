73216c081fb5a7d57198248188901a660faadf3a
Kristy Burge <kristyburge@gmail.com>
2016-12-02 19:47:55 -0700

    add legacy support for responsive image with picturefill and modernizr svg to png

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp'),
     webpack = require('webpack');
 
-gulp.task('scripts', function(callback){
+gulp.task('scripts', ['modernizr'], function(callback){
   webpack(require('../../webpack.config.js'), function(err, stats){
     if (err) {
       console.log(err.toString());
     }
-    
+
     console.log(stats.toString());
     callback();
     /*callback ensures that gulp is aware when webpack completes by passing callback into the function and then run the function in webpack's function */
--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp'),
     svgSprite = require('gulp-svg-sprite'),
     rename = require('gulp-rename'),
-    del = require('del');
+    del = require('del'),
+    svg2png = require('gulp-svg2png');
 
 var config = { // Config for the package svgSprite must be an object literal
   mode: {
     css: {
+      variables: {
+        replaceSvgWithPng: function(){
+          return function(sprite, render){
+            return render(sprite).split('.svg').join('.png');
+          }
+        }
+      },
       sprite: 'sprite.svg', // rename sprite file to remove .css from file name
       render: { // have the package generate the css for the sprites!
         css: { // could use sass, less or stylus in lieu
@@ -26,8 +34,14 @@ gulp.task('createSprite', ['beginClean'], function(){
     .pipe(gulp.dest('./app/temp/sprite/'));
 });
 
-gulp.task('copySpriteGraphic', ['createSprite'], function(){
-  return gulp.src('./app/temp/sprite/css/**/*.svg')
+gulp.task('createPngCopy', ['createSprite'], function(){
+  return gulp.src('./app/temp/sprite/css/*.svg')
+    .pipe(svg2png())
+    .pipe(gulp.dest('./app/temp/sprite/css/'));
+});
+
+gulp.task('copySpriteGraphic', ['createPngCopy'], function(){
+  return gulp.src('./app/temp/sprite/css/**/*.{svg,png}')
     .pipe(gulp.dest('./app/assets/images/sprites/'));
     // above pipe copies to the folder path in the .dest
 });
@@ -42,4 +56,4 @@ gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function(){
   return del('./app/temp/sprite/');
 });
 
-gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteCSS', 'endClean']);
+gulp.task('icons', ['beginClean', 'createSprite', 'createPngCopy', 'copySpriteCSS', 'endClean']);
